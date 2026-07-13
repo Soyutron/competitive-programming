35c8ea46b1263d0bbc58fca75fd6bdd0be39ef25
soyukb <kobayashi.soyu@gmail.com>
2026-07-13 22:52:41 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 
 int main() {
-    int A, B, C;
-    std::cin >> A >> B >> C;
-    bool isA = A == (B + C);
-    bool isB = B == (A + C);
-    bool isC = C == (A + B);
-    bool isABC = (A == B) && (B == C);
-
-    if (isA || isB || isC || isABC) {
-        std::cout << "Yes\n";
+    int A, B;
+    std::cin >> A >> B;
+    int sum = 6;
+    if (A == B) {
+        std::cout << -1 << "\n";
     } else {
-        std::cout << "No\n";
+        std::cout << sum - A - B << "\n";
     }
 }
