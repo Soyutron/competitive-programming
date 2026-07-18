8d1ad2d4c651f6266ead34e00663d728d4219ca2
Soyu Jobs <soyu@example.com>
2026-07-18 16:18:03 +0900

    a

---
+++ atcoder/ABC363/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int R;
+    std::cin >> R;
+    if (1 <= R && R <= 99) {
+        std::cout << 100 - R << "\n";
+    } else if (100 <= R && R <= 199) {
+        std::cout << 200 - R << "\n";
+    } else if (200 <= R && R <= 299) {
+        std::cout << 300 - R << "\n";
+    }
+}
