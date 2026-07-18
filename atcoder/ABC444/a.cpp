ded8856e92f7b6350ef45c129ef01157303ef993
soyukb <kobayashi.soyu@gmail.com>
2026-07-18 19:44:10 +0900

    a

---
+++ atcoder/ABC444/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+    int a = N % 10;
+    int b = N / 10 % 10;
+    int c = N / 100 % 10;
+
+    // std::cout << a << b << c << "\n";
+
+    if (a == b && b == c) {
+        std::cout << "Yes\n";
+    } else {
+        std::cout << "No\n";
+    }
+}
