b5cfd2af661c53d62dee01ce7bf9caf1f8380018
soyukb <kobayashi.soyu@gmail.com>
2026-07-19 01:02:02 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-
+// 361
 int main() {
-    int R;
-    std::cin >> R;
-    if (1 <= R && R <= 99) {
-        std::cout << 100 - R << "\n";
-    } else if (100 <= R && R <= 199) {
-        std::cout << 200 - R << "\n";
-    }
+    int D, F;
+    std::cin >> D >> F;
+    int a = D - F;
+    int answer = ((a / 7) + 1) * 7 - a;
+    std::cout << answer << "\n";
 }
