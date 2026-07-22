659c5912b09b16c75d318b1429bb446837dd25ea
soyukb <kobayashi.soyu@gmail.com>
2026-07-22 23:14:32 +0900

    a

--- main.cpp
+++ main.cpp
@@ -3,16 +3,9 @@
 #include <vector>
 
 int main() {
-    int N, X;
-    std::cin >> N >> X;
-    for (int i = 0; i < N; ++i) {
-        int a;
-        std::cin >> a;
-        if (a < X) {
-            X = a;
-            std::cout << 1 << "\n";
-        } else {
-            std::cout << 0 << "\n";
-        }
-    }
+    std::string S;
+    std::cin >> S;
+    S[0] = std::tolower(S[0]);
+    S.insert(0, "of");
+    std::cout << S << "\n";
 }
